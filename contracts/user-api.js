1b64f232f6cc828b95c13b9ada9222a12fa66d2f
eformat <eformat@gmail.com>
2016-11-12 06:47:29 +1000

    url api parameter

--- contracts/user-api.js
+++ contracts/user-api.js
@@ -2,13 +2,15 @@ var Contract = require('consumer-contracts').Contract;
 var Joi = require('consumer-contracts').Joi;
 var assert = require('chai').assert;
 
+var url = 'http://localhost:8080/people';
+
 describe('Contract HelloWorld', function () {
     it('throws an error when the consumer is missing', function () {
         assert.throws(function () {
             new Contract({
                 name: 'Name',
                 request: {
-                    url: 'http://foo-foo.192.168.137.3.xip.io/people'
+                    url: url
                 },
                 response: {
                     statusCode: 200
@@ -23,7 +25,7 @@ describe('Contract HelloWorld', function () {
             consumer: 'Consumer',
             request: {
                 method: 'POST',
-                url: 'http://foo-foo.192.168.137.3.xip.io/people',
+                url: url,
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
@@ -39,13 +41,57 @@ describe('Contract HelloWorld', function () {
         contract.validate(done)
     });
 
+    it('throws an error when a Person is missing', function (done) {
+        var contract = new Contract({
+            name: 'People',
+            consumer: 'Consumer',
+            request: {
+                method: 'GET',
+                url: url+'/Mikey'
+            },
+            response: {
+                statusCode: 200,
+                body: Joi.array().items(
+                    Joi.object().keys({
+                        name: Joi.string().alphanum().min(4).required(),
+                        message: Joi.array().items(Joi.string().min(1).required()).required()
+                    })
+                )
+            }
+        });
+
+        contract.validate(done)
+    });
+
+    it('throws an error when cant add a Person', function (done) {
+        var contract = new Contract({
+            name: 'People',
+            consumer: 'Consumer',
+            request: {
+                method: 'POST',
+                url: url,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                json: { name: "Fred", message: ["Hello Fred"] }
+            },
+            response: {
+                statusCode: 201
+            }
+        });
+
+        contract.validate(done)
+    });   
+    
     it('throws an error when People are missing', function (done) {
         var contract = new Contract({
             name: 'People',
             consumer: 'Consumer',
             request: {
                 method: 'GET',
-                url: 'http://foo-foo.192.168.137.3.xip.io/people'
+                url: url
             },
             response: {
                 statusCode: 200,
